67055b1913e8417f28114a8bd7c4820b5d40d39f
lakshayraturi <85173668+lakshayraturi@users.noreply.github.com>
2021-07-27 14:04:55 +0530

    Setting up card3

---
+++ lib/card3.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Card3 extends StatelessWidget {
+  const Card3({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints.expand(height: 450, width: 350),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag2.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        child: Stack(
+          children: [
+            // TODO 5: ADD DARK OVERLAY BOXDECORATION
+            // TODO 5: ADD CONTAINER, COLUMN, ICON AND TEXT
+            // TODO 5: ADD CENTER WIDGET WITH CHIP WIDGET CHILDREN
+          ],
+        ),
+      ),
+    );
+  }
+}
